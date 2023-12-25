27ccd78763cb90889705c7a0b5882539a580a128
qwerty1708 <ivasenko083@gmail.com>
2023-12-25 06:34:24 +0200

    3rd lesson

---
+++ pages/views.py
@@ -0,0 +1,7 @@
+from django.views.generic import TemplateView, ListView
+from .models import Post
+class HomePageView(ListView):
+    template_name = 'home.html'
+    model = Post
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
